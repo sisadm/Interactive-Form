8a2e913d51a2c30527b83f09699c928cf8ea5e1a
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2020-08-01 23:18:13 +0100

    Update script.js
    add Validators and Submit event listener

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
+let form = document.querySelector("form");
 let name = document.querySelector('#name');
+let email = document.querySelector('#mail');
 let titles = document.querySelector('#title');
 let otherTitle = document.querySelector('#other-title');
+let shirtDiv = document.querySelector(".shirt-box");
 let tShirtTheme = document.querySelector('#design');
 let tShirtColors = document.querySelector('#color');
+let colorDiv = document.querySelector("#colors-js-puns");
 let colorOptions = document.querySelectorAll('#color option');
 let activities = document.querySelector(".activities");
 let regInputs = document.querySelectorAll(".activities input");
@@ -13,6 +17,11 @@ let paymentDefault = document.querySelector(".paymentDefault");
 let creditCardDiv = document.querySelector("#credit-card");
 let paypalDiv = document.querySelector("#paypal");
 let bitcoinDiv = document.querySelector("#bitcoin");
+let alertActivities = document.querySelector("#alert-activities");
+let cardNumbers = document.querySelector("#cc-num");
+let zipNumbers = document.querySelector("#zip");
+let cvvNumbers = document.querySelector("#cvv");
+let submit = document.querySelector("button");
 
 
 // create total cost part 
@@ -43,7 +52,7 @@ let totalH3 = document.querySelector(".totalMoney");
         }
     });
 
-// T-shirt event listeners
+// T-shirt 
 
     // add color option if nothing select 
     function addDefault() {
@@ -70,13 +79,16 @@ let totalH3 = document.querySelector(".totalMoney");
         if(e.target.value == "js puns" || e.target.value == "heart js") {   
             deleteDefault()
             if(e.target.value == "js puns") {
+                colorDiv.style.display = "inherit";
                 classChange("firstColor");
             }
             if(e.target.value == "heart js") {
+                colorDiv.style.display = "inherit";
                 classChange("secondColor");
             }
         } 
         if(e.target.value == "default" ) {
+            colorDiv.style.display = "none";
             classChange("default");
             addDefault();
         }
@@ -164,6 +176,16 @@ let totalH3 = document.querySelector(".totalMoney");
         } else {
             totalH3.style.display = "none";
         }      
+
+        for(let i = 0; i < regInputs.length; i++) {
+            if(regInputs[i].checked == true) {
+                alertActivities.style.display = "none";            
+                break;
+            } else {
+                alertActivities.style.display = "";
+            }
+
+        }  
     });
 
 // Payment 
@@ -179,8 +201,6 @@ function hideAndShow(value) {
     
 }
 
-    
-
 payment.addEventListener("change", (e) => {
     paymentDefault.style.display = "none";
     let elementValue = e.target.value;
@@ -194,6 +214,214 @@ payment.addEventListener("change", (e) => {
         hideAndShow(elementValue);
     }
 });
+
+// Validators
+
+function validName(username) {
+    return /^[a-z A-Z]+$/.test(username);
+}
+
+function validEmail(email) {
+    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
+}  
+
+function validCardNumbers(numbers) {
+    return /^[0-9]{13,16}$/.test(numbers)
+}
+
+function validZipNumbers(numbers) {
+    return /^[0-9]{5}$/.test(numbers)
+}
+
+function validCvvNumbers(numbers) {
+    return /^[0-9]{3}$/.test(numbers)
+}
+
+// alert message hide or show when user type in the field 
+
+function alertShowOrHide(show, element) {
+    if (show) {
+        element.style.display = "inherit";
+    } else {
+        element.style.display = "none";
+    }
+} 
+
+function createListener(validator) {
+    return e => {
+      const text = e.target.value;
+      const valid = validator(text);
+      const showTip = text !== "" && !valid;
+      const tooltip = e.target.nextElementSibling;
+      alertShowOrHide(showTip, tooltip);
+    };
+  }
+  
+// event listener to check user inputs 
+
+name.addEventListener('input', createListener(validName));
+email.addEventListener('input', createListener(validEmail));
+cardNumbers.addEventListener('input', createListener(validCardNumbers));
+zipNumbers.addEventListener('input', createListener(validZipNumbers));
+cvvNumbers.addEventListener('input', createListener(validCvvNumbers));
+
+
+// create Ul which will contain any error Li
+let ul = document.createElement("ul");
+ul.setAttribute("id", "show-error");
+form.appendChild(ul);
+
+// event listener to submit the form and check everything is correct
+
+submit.addEventListener('click', () => {
+    let li;
+    let errorUl = document.querySelector("#show-error");
+    let nameLi = document.querySelector(".nameError");
+    let emailLi = document.querySelector(".emailError");
+    let otherLi = document.querySelector(".otherTitleError");
+    let actLi = document.querySelector(".actError");
+    let tshirtLi = document.querySelector(".tshirtError");
+    let cardNumLi = document.querySelector(".cardNumError");
+    let zipNumLi = document.querySelector(".zipNumError");
+    let cvvNumLi = document.querySelector(".cvvNumError");
+
+    // name check
+    if(name.value == "") {
+        li =  document.createElement("li");
+        li.classList.add("nameError");
+        li.innerHTML = "*Please provide a name."
+        if(!nameLi) {
+            errorUl.appendChild(li);
+            name.classList.add("red-border");
+        }
+    } else {
+        if(nameLi) {
+            nameLi.remove();
+            name.classList.remove("red-border");
+        }
+        
+    }
+
+    //email check
+    let emailAlert = document.querySelector("#mail + .alert-message");
+    if(email.value == "" || emailAlert.style.display !== "none") {
+        li =  document.createElement("li");
+        li.classList.add("emailError");
+        li.innerHTML = "*Please fill the form with correct email.";
+        if(!emailLi) {
+            errorUl.appendChild(li);
+            email.classList.add("red-border");
+        }
+    } else {
+        if(emailLi) {
+            emailLi.remove();
+            email.classList.remove("red-border");
+        }   
+    }
+
+    //job check
+    if(titles.value == "other" && otherTitle.value == "") {
+        li =  document.createElement("li");
+        li.classList.add("otherTitleError");
+        li.innerHTML = "*Please fill the other Job field.";
+        if(!otherLi) {
+            errorUl.appendChild(li);
+            otherTitle.classList.add("red-border");
+        }
+    } else {
+        if(otherLi) {
+            otherLi.remove();
+            otherTitle.classList.remove("red-border");
+        }   
+    }
+
+ 
+    // t-shirt check
+
+    if(tShirtColors.value == "default") {
+        li =  document.createElement("li");
+        li.classList.add("tshirtError");
+        li.innerHTML = "*Please choose one T-shirt.";
+        if(!tshirtLi) {
+            errorUl.appendChild(li);
+            tShirtTheme.classList.add("red-border");
+        }
+    } else {
+        if(tshirtLi) {
+            tshirtLi.remove();
+            tShirtTheme.classList.remove("red-border");
+        }   
+    }
+
+    // activities check
+    let activitiesAlert = document.querySelector("#alert-activities");
+    if(activitiesAlert.style.display !== "none") {
+        li =  document.createElement("li");
+        li.classList.add("actError");
+        li.innerHTML = "*Please choose at least one from Activities.";
+        if(!actLi) {
+            errorUl.appendChild(li);
+        }
+    } else {
+        if(actLi) {
+            actLi.remove();
+        }   
+    }    
+    
+    // card check 
+    if(cardNumbers.value.length < 13 || cardNumbers.value.length  > 16) {
+        let cardLength = cardNumbers.value.length;
+        li =  document.createElement("li");
+        li.classList.add("cardNumError");
+        li.innerHTML = `*Card numbers need to be 13 - 16 digits. You enter ${cardLength} digits.`;
+        if(!cardNumLi) {
+            errorUl.appendChild(li);
+            cardNumbers.classList.add("red-border");
+        }
+    } else {
+        if(cardNumLi) {
+            cardNumLi.remove();
+            cardNumbers.classList.remove("red-border");
+        }   
+    }   
+    
+    // zip check
+    if(zipNumbers.value.length < 5 || zipNumbers.value.length  > 5) {
+        let zipLength = cardNumbers.value.length;
+        li =  document.createElement("li");
+        li.classList.add("zipNumError");
+        li.innerHTML = `*Zip Code need to be 5 digits. You enter ${zipLength} digits.`;
+        if(!zipNumLi) {
+            errorUl.appendChild(li);
+            zipNumbers.classList.add("red-border");
+        }
+    } else {
+        if(zipNumLi) {
+            zipNumLi.remove();
+            zipNumbers.classList.remove("red-border");
+        }   
+    }
+    
+    // CVV check
+    if(cvvNumbers.value.length !== 3) {
+        let cvvLength = cvvNumbers.value.length;
+        li =  document.createElement("li");
+        li.classList.add("cvvNumError");
+        li.innerHTML = `*CVV need to be 3 digits. You enter ${cvvLength} digits.`;
+        if(!cvvNumLi) {
+            errorUl.appendChild(li);
+            cvvNumbers.classList.add("red-border");
+        }
+    } else {
+        if(cvvNumLi) {
+            cvvNumLi.remove();
+            cvvNumbers.classList.remove("red-border");
+        }   
+    } 
+
+
+});
+
 
 
 //call the functions to default mode
